ac68c1388d47d68651c6fc727f1e8b7f42f9cd11
Sarah West <west0883@umn.edu>
2023-07-27 10:58:32 -0500

    call to make duration_place for single timepoint fluorescence PSLR

--- pipeline_duration_place_concatenation.m
+++ pipeline_duration_place_concatenation.m
@@ -28,12 +28,182 @@ parameters.mice_all = mice_all;
 parameters.mice_all = parameters.mice_all;
 % 
 
+% Load names of motorized periods
+load([parameters.dir_exper 'periods_nametable.mat']);
+periods_motorized = periods;
+
+% Load names of spontaneous periods
+load([parameters.dir_exper 'periods_nametable_spontaneous.mat']);
+periods_spontaneous = periods(1:6, :);
+clear periods; 
+
+% Create a shared motorized & spontaneous list.
+periods_bothConditions = [periods_motorized; periods_spontaneous]; 
+parameters.periods_bothConditions = periods_bothConditions; 
+
 % Give the number of digits that should be included in each stack number.
 parameters.digitNumber=2; 
 
 parameters.loop_variables.periods = {'rest', 'walk'}; % spontaneous continued periods 
 parameters.loop_variables.mice_all = parameters.mice_all;
 parameters.loop_variables.conditions =   {'motorized'; 'spontaneous'};
+parameters.loop_variables.conditions_stack_locations =  {'stacks'; 'spontaneous'};
+parameters.loop_variables.periods_bothConditions = periods_bothConditions.condition;
+
+%% Motorized: Look for stacks when number of instances don't match those of fluorescence.
+% (Just do motorized rest, that's the one you're having problems with).
+% Don't need to load, so don't use RunAnalysis.
+% Only need to run 1 body part & velocity direction (FL, total magnitude).
+
+parameters.loop_list.iterators = {
+               'mouse', {'loop_variables.mice_all(:).name'}, 'mouse_iterator'; 
+               'day', {'loop_variables.mice_all(', 'mouse_iterator', ').days(:).name'}, 'day_iterator';
+               'stack', {'loop_variables.mice_all(',  'mouse_iterator', ').days(', 'day_iterator', ').stacks'}, 'stack_iterator';
+               };
+
+% If using a sub-structure, need to use regular loading
+parameters.use_substructure = true; 
+
+parameters.checkingDim = 1;
+parameters.check_againstDim = 3;
+
+% Input values
+parameters.loop_list.things_to_check.dir = {[parameters.dir_exper 'behavior\motorized\period instances table format\'], 'mouse', '\', 'day', '\'};
+parameters.loop_list.things_to_check.filename= {'all_periods_', 'stack', '.mat'};
+parameters.loop_list.things_to_check.variable= {'all_periods.duration_place(176:180)'}; 
+parameters.loop_list.things_to_check.level = 'stack';
+
+parameters.loop_list.check_against.dir = {[parameters.dir_exper 'fluorescence analysis\segmented timeseries\motorized\'],  'mouse', '\', 'day', '\'};
+parameters.loop_list.check_against.filename= {'segmented_timeseries_', 'stack', '.mat'};  
+parameters.loop_list.check_against.variable = {'segmented_timeseries(176:180)'};
+
+% Output
+parameters.loop_list.mismatched_data.dir = {[parameters.dir_exper 'behavior\duration place concatenated\']};
+parameters.loop_list.mismatched_data.filename= {'mismatched_data_motorized.mat'};
+
+CheckSizes2(parameters);
+
+%% Spontaneous: Look for stacks when number of instances don't match those of fluorescence.
+% (Just do motorized rest, that's the one you're having problems with).
+% Don't need to load, so don't use RunAnalysis.
+% Only need to run 1 body part & velocity direction (FL, total magnitude).
+
+parameters.loop_list.iterators = {
+               'mouse', {'loop_variables.mice_all(:).name'}, 'mouse_iterator'; 
+               'day', {'loop_variables.mice_all(', 'mouse_iterator', ').days(:).name'}, 'day_iterator';
+               'stack', {'loop_variables.mice_all(',  'mouse_iterator', ').days(', 'day_iterator', ').spontaneous'}, 'stack_iterator';
+               'period', {'loop_variables.periods'}, 'period_iterator';
+               };
+
+% If using a sub-structure, need to use regular loading
+parameters.use_substructure = true; 
+
+parameters.checkingDim = 1;
+parameters.check_againstDim = 3;
+
+% Input values
+parameters.loop_list.things_to_check.dir = {[parameters.dir_exper 'behavior\spontaneous\segmented behavior periods\'], 'mouse', '\', 'day', '\'};
+parameters.loop_list.things_to_check.filename= {'duration_places_', 'stack', '.mat'};
+parameters.loop_list.things_to_check.variable= {'duration_places.', 'period'}; 
+parameters.loop_list.things_to_check.level = 'stack';
+
+parameters.loop_list.check_against.dir = {[parameters.dir_exper 'fluorescence analysis\segmented timeseries\spontaneous\'],  'mouse', '\', 'day', '\'};
+parameters.loop_list.check_against.filename= {'segmented_timeseries_', 'stack', '.mat'};  
+parameters.loop_list.check_against.variable = {'segmented_timeseries(', 'period_iterator', ')'};
+
+% Output
+parameters.loop_list.mismatched_data.dir = {[parameters.dir_exper 'behavior\duration place concatenated\']};
+parameters.loop_list.mismatched_data.filename= {'mismatched_data_spontaneous.mat'};
+
+CheckSizes2(parameters);
+%% Notes for removal.
+
+% For these, are okay to just take all but the end instances 
+
+% motorized 
+
+% load mismatched data 
+load('Y:\Sarah\Analysis\Experiments\Random Motorized Treadmill\behavior\duration place concatenated\mismatched_data_motorized.mat')
+
+% For each item in mismatched data, 
+for itemi = 2:size(mismatched_data, 1)
+   
+    % load the duration data
+    load(['Y:\Sarah\Analysis\Experiments\Random Motorized Treadmill\behavior\motorized\period instances table format\' mismatched_data{itemi, 1} '\' mismatched_data{itemi, 2} '\all_periods_' mismatched_data{itemi, 3} '.mat'])
+    duration = all_periods.duration_place(176:180);
+    
+    % load the flourescence data 
+    load(['Y:\Sarah\Analysis\Experiments\Random Motorized Treadmill\fluorescence analysis\segmented timeseries\motorized\' mismatched_data{itemi, 1} '\' mismatched_data{itemi, 2} '\segmented_timeseries_' mismatched_data{itemi, 3} '.mat'])
+    fluor = segmented_timeseries;
+
+    % find number of instances you should have
+    % if empty, make it set = 0 (because 3rd dim will be 1)
+    if  ~isempty(fluor{mismatched_data{itemi, 4} + 175})
+        correct_num = size(fluor{mismatched_data{itemi, 4} + 175}, 3);
+    else
+        correct_num = 0;
+    end
+    duration_new = duration;
+    duration_new{mismatched_data{itemi, 4}} = duration{mismatched_data{itemi, 4}}(1:correct_num);
+
+    % Save over previous 
+    all_periods.duration_place(176:180) = duration_new;
+    save(['Y:\Sarah\Analysis\Experiments\Random Motorized Treadmill\behavior\motorized\period instances table format\' mismatched_data{itemi, 1} '\' mismatched_data{itemi, 2} '\all_periods_' mismatched_data{itemi, 3} '.mat'], 'all_periods');
+
+end 
+
+
+%% correct mismatches in spontaneous 
+
+% load mismatched data 
+load('Y:\Sarah\Analysis\Experiments\Random Motorized Treadmill\behavior\duration place concatenated\mismatched_data_spontaneous.mat')
+
+% For each item in mismatched data, 
+for itemi = 2:size(mismatched_data, 1)
+   
+    % load the duration data
+    load(['Y:\Sarah\Analysis\Experiments\Random Motorized Treadmill\behavior\spontaneous\segmented behavior periods\' mismatched_data{itemi, 1} '\' mismatched_data{itemi, 2} '\duration_places_' mismatched_data{itemi, 3} '.mat'])
+    duration = duration_places;
+    
+    % load the flourescence data 
+    load(['Y:\Sarah\Analysis\Experiments\Random Motorized Treadmill\fluorescence analysis\segmented timeseries\spontaneous\' mismatched_data{itemi, 1} '\' mismatched_data{itemi, 2} '\segmented_timeseries_' mismatched_data{itemi, 3} '.mat'])
+    fluor = segmented_timeseries;
+
+    % find number of instances you should have
+    % if empty, make it set = 0 (because 3rd dim will be 1)
+    if  ~isempty(fluor{mismatched_data{itemi, 5}})
+        correct_num = size(fluor{mismatched_data{itemi, 5}}, 3);
+    else
+        correct_num = 0;
+    end
+    duration_new = duration;
+    duration_new.(mismatched_data{itemi, 4}) = duration.(mismatched_data{itemi, 4})(1:correct_num);
+
+    % Save over previous 
+    duration_places = duration_new;
+    save(['Y:\Sarah\Analysis\Experiments\Random Motorized Treadmill\behavior\spontaneous\segmented behavior periods\' mismatched_data{itemi, 1} '\' mismatched_data{itemi, 2} '\duration_places_' mismatched_data{itemi, 3} '.mat'], 'duration_places')
+
+end 
+
+%From first round:
+% '1087'	'011222'	'motorized'	'11'	180
+% --> get rid of the last instance, the fluoresence was short by ~20 frames
+% load('Y:\Sarah\Analysis\Experiments\Random Motorized Treadmill\behavior\motorized\period instances table format\1087\011222\all_periods_11.mat');
+% all_periods.duration_place{180} = all_periods.duration_place{180}(1:end-1);
+% save('Y:\Sarah\Analysis\Experiments\Random Motorized Treadmill\behavior\motorized\period instances table format\1087\011222\all_periods_11.mat', 'all_periods');
+% 
+% %'1088'	'010522'	'motorized'	'11'	180 
+% % --> same as above
+% load('Y:\Sarah\Analysis\Experiments\Random Motorized Treadmill\behavior\motorized\period instances table format\1088\010522\all_periods_11.mat');
+% all_periods.duration_place{180} = all_periods.duration_place{180}(1:end-1);
+% save('Y:\Sarah\Analysis\Experiments\Random Motorized Treadmill\behavior\motorized\period instances table format\1088\010522\all_periods_11.mat', 'all_periods');
+% 
+% % '1088'	'011322'	'motorized'	'06'	180
+% % short by 2 frames; remove last 2 instances. 
+% load('Y:\Sarah\Analysis\Experiments\Random Motorized Treadmill\behavior\motorized\period instances table format\1088\011322\all_periods_06.mat');
+% all_periods.duration_place{180} = all_periods.duration_place{180}(1:end-2);
+% load('Y:\Sarah\Analysis\Experiments\Random Motorized Treadmill\behavior\motorized\period instances table format\1088\011322\all_periods_06.mat', 'all_periods');
+
 
 %% Motorized 
 
@@ -162,3 +332,32 @@ parameters.loop_list.things_to_save.concatenated_data.level = 'mouse';
 RunAnalysis({@ConcatenateData}, parameters);
 
 parameters.concatenate_across_cells = false;
+
+%%  Make duration_place for fluorescence PLSR 
+% (Single time point)
+% Always clear loop list first. 
+if isfield(parameters, 'loop_list')
+parameters = rmfield(parameters,'loop_list');
+end
+
+% Iterators
+parameters.loop_list.iterators = {'mouse', {'loop_variables.mice_all(:).name'}, 'mouse_iterator'; 
+               'period', {'loop_variables.periods_bothConditions'}, 'period_iterator';
+               };
+
+parameters.window_length = 20; % window length in time points
+parameters.fps = 20; % Frame per second of the fluorescence
+
+% Input
+parameters.loop_list.things_to_load.data.dir = {[parameters.dir_exper 'behavior\duration place concatenated\both conditions\'], 'mouse', '\'};
+parameters.loop_list.things_to_load.data.filename= {'all_duration_place.mat'};
+parameters.loop_list.things_to_load.data.variable= {'all_duration_place{', 'period_iterator', '}'}; 
+parameters.loop_list.things_to_load.data.level = 'mouse';
+
+% Output
+parameters.loop_list.things_to_save.duration_place_new.dir = {[parameters.dir_exper 'behavior\duration place concatenated\duration place for fluorescence PLSR\'], 'mouse', '\'};
+parameters.loop_list.things_to_save.duration_place_new.filename= {'duration_place_forFluorescence.mat'};
+parameters.loop_list.things_to_save.duration_place_new.variable= {'duration_place_forFluorescence{', 'period_iterator', ', 1}'}; 
+parameters.loop_list.things_to_save.duration_place_new.level = 'mouse';
+
+RunAnalysis({@DurationPlaceForFluorescencePLSR}, parameters);
